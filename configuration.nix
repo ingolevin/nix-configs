271154bf490149864b4cdea3ac0480de1bd0729c
Ingo Levin <ingo.levin@m2dot.com>
2025-04-17 16:27:00 +0200

    feat: Define disko config inline in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, inputs, diskoConfigPath, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
 {
   imports = [
@@ -9,8 +9,63 @@
     ./modules/hyperv-guest.nix
   ];
 
-  # Explicitly import disko config using path from specialArgs
-  disko.config = import diskoConfigPath;
+  # Define disko config inline
+  disko.config = {
+    devices = {
+      disk.main = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "513MiB";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            }
+            {
+              name = "luks";
+              start = "513MiB";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "cryptlvm";
+                content = {
+                  type = "lvm";
+                  lvs = [
+                    {
+                      name = "swap";
+                      size = "8G";
+                      content = {
+                        type = "swap";
+                        resumeDevice = true;
+                      };
+                    }
+                    {
+                      name = "root";
+                      size = "100%FREE";
+                      content = {
+                        type = "filesystem";
+                        format = "ext4";
+                        mountpoint = "/";
+                      };
+                    }
+                  ];
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
 
   # Boot loader configuration
   boot.loader.grub = {
