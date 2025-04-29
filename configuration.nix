c8ccd62f23b5c2879c006f120d8a82f250513b99
Ingo Levin <ingo.levin@m2dot.com>
2025-04-29 16:39:54 +0200

    Remove legacy bitcoin-knots service. Use new implementation option

--- configuration.nix
+++ configuration.nix
@@ -14,18 +14,13 @@
   # Configure nix-bitcoin
   nix-bitcoin.generateSecrets = true;
 
-  # Disable bitcoind from upstream nix-bitcoin
-  services.bitcoind.enable = false;
-
-  # Enable bitcoin-knots from the fork
-  services.bitcoin-knots = {
+  # Enable bitcoind from upstream nix-bitcoin
+  services.bitcoind = {
     enable = true;
-    # Prune blockchain data, keep only ~10GB
-    # Use knotsSpecificOptions as defined in the fork's module
-    knotsSpecificOptions = {
-      prune = "10000";
-    };
+    implementation = "knots";
+    prune = 10000;
   };
+
 
   # Boot loader configuration
   boot.loader.systemd-boot = { enable = true; };
